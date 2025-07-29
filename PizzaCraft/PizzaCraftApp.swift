85fc7acb8b5a4d3cf473f78e34ea90c0349178c0
KrunalMaisuriya929 <krunalmaisuriya929@gmail.com>
2025-07-29 13:18:48 +0100

    Initial Commit

---
+++ PizzaCraft/PizzaCraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaCraftApp.swift
+//  PizzaCraft
+//
+//  Created by Krunal on 29/07/25.
+//
+
+import SwiftUI
+
+@main
+struct PizzaCraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
